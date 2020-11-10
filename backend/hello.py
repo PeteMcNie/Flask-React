7f5a013b357de2d7998b7e77385275a931439aaa
petemcnie <petemcnie0@gmail.com>
2020-11-11 11:33:20 +1300

    Add jsonify to return

--- backend/hello.py
+++ backend/hello.py
@@ -1,7 +1,10 @@
-from flask import Flask
+from flask import (
+    Flask, jsonify, request
+)
+
 app = Flask(__name__)
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET'])
 def hello():
     myDict = { "name": 'pete', "stat": 'defence' }
-    return myDict
+    return jsonify(myDict)
