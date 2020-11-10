654877c4e9043a13a3046662e73988f8234de3ad
petemcnie <petemcnie0@gmail.com>
2020-11-10 14:44:30 +1300

    Create server and public files

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path')
+const express = require('express')
+
+const server = express()
+
+server.use(express.json())
+server.use(express.static(path.join(__dirname, './public')))
+
+// MORE ROUTES HERE
+
+server.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
+
+module.exports = server
